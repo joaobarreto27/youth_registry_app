c00da30b718690f75a56a110428e6708c03313e5
joaobarreto27 <joao.vito1951@gmail.com>
2026-03-27 09:21:15 -0300

    feat(app): melhora experiência de ux do usuário, passando erro ao verificar saúde da api para o backend

--- src/frontend/app/app.py
+++ src/frontend/app/app.py
@@ -186,9 +186,6 @@ if not st.session_state.api_awake:
 # ==================== INTERFACE STREAMLIT ====================
 def main():
     if not st.session_state.get("api_awake"):
-        st.warning(
-            "⚠️ A API não está disponível. Por favor, tente novamente mais tarde."
-        )
         st.stop()
 
     saved_token = None
